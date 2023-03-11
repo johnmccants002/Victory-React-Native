ca9efa3680ce588f5c31dc5cae0855c07c01247e
John McCants <johnmccants002@gmail.com>
2023-03-10 19:24:56 -0800

    Styled Post Objects

--- src/screens/VictoryMainFeed.tsx
+++ src/screens/VictoryMainFeed.tsx
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, ScrollView, Image, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import { Victory } from "../models/index";
 import { DataStore } from "aws-amplify";
 import { Skeleton } from "moti/skeleton";
+import { Dimensions } from "react-native";
+import { data } from "../data/dummyData";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export const VictoryMainFeed = () => {
   const [victories, setVictories] = useState<Array<Victory>>([]);
   const [isLoading, setIsLoading] = useState(true);
   const skelData = [1, 2, 3, 4, 5, 6, 7, 8];
+  const screenWidth = Dimensions.get("window").width;
 
   useEffect(() => {
     fetchVictories();
@@ -23,6 +34,10 @@ export const VictoryMainFeed = () => {
     setVictories(victories);
   };
 
+  const replyPress = () => {
+    console.log("reply button pressed");
+  };
+
   const renderSkeletonPosts = () => {
     return skelData.map(() => (
       <View style={{ padding: 20 }}>
@@ -30,7 +45,7 @@ export const VictoryMainFeed = () => {
           height={200}
           width={300}
           colorMode={"light"}
-          colors={["gainsboro", "lightGray", "black"]}
+          colors={["gainsboro", "lightGray"]}
         />
       </View>
     ));
@@ -44,8 +59,106 @@ export const VictoryMainFeed = () => {
         flex: 1,
       }}
     >
-      <ScrollView showsVerticalScrollIndicator={false}>
-        {isLoading ? (
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          alignItems: "center",
+          justifyContent: "center",
+          width: screenWidth,
+        }}
+      >
+        {data.map((obj) => (
+          <View
+            style={{
+              // flexDirection: "column",
+              backgroundColor: "#FFFFFF",
+              borderRadius: 10,
+              elevation: 3,
+              margin: 8,
+              padding: 12,
+              shadowColor: "#000000",
+              shadowOffset: {
+                width: 0,
+                height: 2,
+              },
+              shadowOpacity: 0.1,
+              shadowRadius: 2,
+              width: "80%",
+              height: 240,
+            }}
+          >
+            <View style={{ flexDirection: "row" }}>
+              <Image
+                style={{ height: 40, width: 40 }}
+                source={{
+                  uri: "https://allworldpm.com/wp-content/uploads/2016/10/230x230-avatar-dummy-profile-pic.jpg",
+                }}
+              />
+
+              <View style={{ flexDirection: "column" }}>
+                <Text>{obj.name}</Text>
+                <Text>@{obj.username}</Text>
+              </View>
+            </View>
+            <View>
+              <View
+                style={{
+                  flexDirection: "column",
+                  width: "auto",
+                  height: 150,
+                }}
+              >
+                {obj.postImage && (
+                  <View
+                    style={{
+                      width: "auto",
+                      height: 120,
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Image
+                      style={{
+                        width: "100%",
+                        height: 100,
+                        paddingHorizontal: 8,
+                      }}
+                      source={{ uri: obj.postImage }}
+                    />
+                  </View>
+                )}
+                <Text>{obj.postText}</Text>
+                <View
+                  style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <View style={styles.divider}></View>
+                </View>
+                <TouchableOpacity onPress={replyPress}>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      paddingTop: 4,
+                    }}
+                  >
+                    <Text>Reply</Text>
+                    <MaterialCommunityIcons
+                      name={"message-outline"}
+                      size={25}
+                      color={"grey"}
+                    />
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        ))}
+
+        {/* {isLoading ? (
           renderSkeletonPosts()
         ) : victories ? (
           <>
@@ -75,7 +188,7 @@ export const VictoryMainFeed = () => {
           </>
         ) : (
           <Text>Fetching Victories</Text>
-        )}
+        )} */}
       </ScrollView>
     </View>
   );
@@ -87,6 +200,12 @@ const styles = StyleSheet.create({
     height: 80,
     resizeMode: "contain",
     marginBottom: 16,
+  },
+  divider: {
+    height: 1,
+    width: "95%",
+    backgroundColor: "gainsboro",
+    marginTop: 8,
   },
 });
 
